8598358eee7b19af6dd42b73db74efbf1ea81b11
Samikce <32225472+Samikce@users.noreply.github.com>
2021-06-07 14:49:05 +0530

    Create Task2.js

---
+++ Task2.js
@@ -0,0 +1,8 @@
+for (var i = 1; i <= 5; ++i) {
+    var c=7;
+          for (var j = 0; j <= i; ++j) {
+             document.write(c+" ")
+             c=c-1;
+          }
+         document.write("<br>")
+       }
