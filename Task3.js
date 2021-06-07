eb7ad020f56c849acaaa2b944dbe9cb1adaa006e
Samikce <32225472+Samikce@users.noreply.github.com>
2021-06-07 14:47:41 +0530

    Create Task3.js

---
+++ Task3.js
@@ -0,0 +1,11 @@
+var date = prompt("Enter the date!");
+var arr=date.split("/")
+var year = arr[2]
+if ((year%4 == 0) && (year%100 != 0 )|| (year%400 == 0))
+{
+    alert(year+" is a leap year ");
+}
+else
+{
+    alert(year+" is not a leap year ");
+}
